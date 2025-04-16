66eb2e3da4bb2c36ad3c959672299f43e991abd6
Phani-stack <kdlphani916@gmail.com>
2025-04-16 16:57:16 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql';
+import 'dotenv/config'
 
 const connection = mysql.createConnection({
-   host : `localhost`,
-   user : `root`,
-   password : ``,
-   database : `eapcet_cutoffs`
+   host : process.env.HOST,
+   user : process.env.USER,
+   password: process.env.PASSWORD,
+   database: process.env.DATABASE
 })
 
 connection.connect((err) => {
